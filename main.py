8d210c971560749c73b587b07db91d823eae59a8
tzipiShtrochlitz <t0556786330@gmail.com>
2022-07-31 13:19:29 +0300

    start

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import ctypes
+import os
+import sys
+from sys import platform
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_path_of_hosts():
+    if platform == "linux" or platform == "linux2":
+        return "/etc/hosts"
+    elif platform == "win32":
+        return "C:\Windows\System32\drivers\etc\hosts"
+
+
+def get_list(*lst):
+    new_lst=[]
+    for i in lst:
+        try:
+            begin=str.index(i,"//",0)+2
+        except:
+            begin = 0
+        try:
+            end=str.index(i,"/",begin)
+        except:
+            end=-1
+        new_lst.append(i[begin:end])
+    return new_lst
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def add_sites(*lst):
+    sites = get_list(*lst)
+    to_add=set(sites).difference(blocked)
+    path = get_path_of_hosts()
+    try:
+        if(not is_admin()):
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        with open(path,'a') as file:
+            for i in to_add:
+                file.write(i+'\t')
+        for i in to_add:
+            blocked.add(i)
+            print(i," is blocked")
+    except:
+        print("was an error")
+
+
+
+
+
+
+
+#get the blocked web sites
+blocked=set()
+flag=False
+with open(get_path_of_hosts(),'r+') as file:
+    for i in file.read().split('\n'):
+        if i[0:9] == '127.0.0.1':
+            blocked=set(i[str.index(i,'\t'):].split('\t'))
+            flag=True
+    if flag==False:
+        file.write('\n127.0.0.1\t')
+
+add_sites("https://www.meuhedet.co.il/","https://www.hhh.co.il")
