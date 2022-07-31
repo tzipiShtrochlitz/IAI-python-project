479c738662e1b28e67fabf741fd77b93202c2f6d
tzipiShtrochlitz <t0556786330@gmail.com>
2022-07-31 20:41:23 +0300

    unblock

--- main.py
+++ main.py
@@ -10,19 +10,16 @@ def get_path_of_hosts():
         return "C:\Windows\System32\drivers\etc\hosts"
 
 
-def get_list(*lst):
-    new_lst=[]
-    for i in lst:
-        try:
-            begin=str.index(i,"//",0)+2
-        except:
-            begin = 0
-        try:
-            end=str.index(i,"/",begin)
-        except:
-            end=len(i)
-        new_lst.append(i[begin:end])
-    return new_lst
+def split_name(full):
+    try:
+        begin=str.index(full,"//",0)+2
+    except:
+        begin = 0
+    try:
+        end=str.index(full,"/",begin)
+    except:
+        end=len(full)
+    return full[begin:end]
 
 def is_admin():
     try:
@@ -31,7 +28,9 @@ def is_admin():
         return False
 
 def add_sites(*lst):
-    sites = get_list(*lst)
+    sites = []
+    for i in lst:
+        sites.append(split_name(i))
     to_add=set(sites).difference(blocked)
     path = get_path_of_hosts()
     try:
@@ -45,6 +44,22 @@ def add_sites(*lst):
             print(i," is blocked")
     except:
         print("was an error")
+
+def unblock(name):
+    new_text=""
+    name=split_name(name)
+    with open(get_path_of_hosts(), 'r') as file:
+        text = file.read().split('\n')
+    for i in text:
+        if i[0:9]=='127.0.0.1':
+            if name in i.split('\t'):
+                i=i.replace(name+'\t','')
+        if len(i)>1:
+            new_text=new_text+'\n'+i
+    with open(get_path_of_hosts(),'w') as file:
+        file.write(new_text)
+    print(name," is unblocked")
+
 
 #get the blocked web sites
 blocked=set()
@@ -61,3 +76,4 @@ with open(get_path_of_hosts(),'r+') as file:
         file.write('\n127.0.0.1\t')
 
 add_sites("https://www.meuhedet.co.il/","https://www.hhh.co.il")
+unblock("https://www.meuhedet.co.il/")
