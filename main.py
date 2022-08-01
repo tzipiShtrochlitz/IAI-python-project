10ee876998c5880bd57cce7fd556fa09f49d5ec2
tzipiShtrochlitz <t0556786330@gmail.com>
2022-08-01 14:27:38 +0300

    ש

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-
-# !/usr/bin/python
 import ctypes
 import os
 import sys
 from sys import platform
 import argparse
-
-
 parser = argparse.ArgumentParser()
-
 parser.add_argument('--block','-b', default=[],help="enter sites to block",nargs='*')
 parser.add_argument('--unblock','-ub', default=[],help="enter sites to unblock",nargs='*')
 args = parser.parse_args()
-
-
 def get_path_of_hosts():
     if platform == "linux" or platform == "linux2":
         return "/etc/hosts"
     elif platform == "win32":
         return "C:\Windows\System32\drivers\etc\hosts"
-
 def split_name(full):
     try:
         begin=str.index(full,"//",0)+2
@@ -30,32 +22,25 @@ def split_name(full):
     except:
         end=len(full)
     return full[begin:end]
-
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
-def block_sites(*lst):
+def block_sites(lst):
+    print(lst)
     sites = []
     for i in lst:
         sites.append(split_name(i))
     to_add=set(sites).difference(blocked)
     path = get_path_of_hosts()
-    try:
-        if(not is_admin()):
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-        with open(path,'a') as file:
-            for i in to_add:
-                file.write(i+'\t')
+    with open(path, 'a') as file:
         for i in to_add:
-            blocked.add(i)
-            print(i," is blocked")
-    except:
-        print("was an error")
-
-def unblock(*sites):
+            file.write(i + '\t')
+    for i in to_add:
+        blocked.add(i)
+        print(i, " is blocked")
+def unblock(sites):
     for name in sites:
         new_text=""
         name=split_name(name)
@@ -70,13 +55,16 @@ def unblock(*sites):
         with open(get_path_of_hosts(),'w') as file:
             file.write(new_text)
         print(name," is unblocked")
-
-
 #get the blocked web sites
 blocked=set()
 #if there are blocked web-sites already
 flag=False
 #find the blocked web-sites
+try:
+    if (not is_admin()):
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+except:
+        print("was an error")
 with open(get_path_of_hosts(),'r+') as file:
     text=file.read().split('\n')
     for i in text:
@@ -88,4 +76,4 @@ with open(get_path_of_hosts(),'r+') as file:
 if len(args.block)>0:
     block_sites(args.block)
 if len(args.unblock) > 0:
-        unblock(i)
+    unblock(args.unblock)
